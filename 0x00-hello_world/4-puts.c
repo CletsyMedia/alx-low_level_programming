89ab63c424bf9030bdb1e44558a1b853c499e2a6
CletsyMedia <Cletussam12@yahoo.com>
2023-06-15 15:27:12 +0100

    A C program that prints exactly

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a line of code using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
