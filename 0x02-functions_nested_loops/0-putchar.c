18200d4debca15996a7292cb95c649b73f55bc66
CletsyMedia <Cletussam12@yahoo.com>
2023-06-18 14:44:50 +0100

    Print the word _putchar using the putchar()

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main - Print the word _putchar using the putchar() function
+ *
+ *  Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
+
