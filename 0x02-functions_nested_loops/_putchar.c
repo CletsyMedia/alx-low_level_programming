cc62a83af5c8bc19ab9462967404f7c5a4ee6aab
CletsyMedia <Cletussam12@yahoo.com>
2023-06-19 05:43:22 +0100

    C Header Boilerplates for _putchar()

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @c:The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef main_h
 #define main_h
-
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,5 +13,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
 #endif
