ed2b3869b1784c96ae0b7e8bd0924b407d45b3cf
CletsyMedia <Cletussam12@yahoo.com>
2023-06-20 12:34:26 +0100

    Update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,16 @@
  */
 int print_last_digit(int n)
 {
+	int a;
+
 	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+	_putchar(a + '0');
+
+	return (a);
 }
