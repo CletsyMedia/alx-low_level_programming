4c2b3181f6a03538cc20a8880e199f482b16e1e3
CletsyMedia <Cletussam12@yahoo.com>
2023-06-20 14:15:25 +0100

    Prints number(s) from 0 t0 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,17 +7,25 @@
  */
 void print_to_98(int n)
 {
+	int i, j;
+
 	if (n <= 98)
 	{
-		for (n = 0; n <= 98; n++)
-			printf("%d, ", n);
-		printf("%d\n", 98);
-	}
-	else
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
 	{
-		for (n = n; n >= 98; n--)
-			printf("%d, ", n);
-		printf("%d\n", 98);
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
 	}
-	
 }
