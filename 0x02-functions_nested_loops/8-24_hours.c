ea6a815fb165817afb624c4c9c2727b5ebd025a8
CletsyMedia <Cletussam12@yahoo.com>
2023-06-20 12:49:51 +0100

    The day of Jack Bauer, starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * @jack_bauer: - A program prints the time table in this format: HH:MM
+ * jack_bauer - A program prints the time table in this format: HH:MM
  * Return: the time in the sum of two numbers.
  */
 void jack_bauer(void)
@@ -9,7 +9,7 @@ void jack_bauer(void)
 
 	for (hrs = 0; hrs < 24; hrs++)
 	{
-		for (mins = 0; mins < 60; m++)
+		for (mins = 0; mins < 60; mins++)
 		{
 			_putchar((hrs / 10) + '0');
 			_putchar((hrs % 10) + '0');
