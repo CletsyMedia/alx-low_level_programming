ad411ea0f08a82acaa69101e415fa2ae6c4d9f4d
CletsyMedia <Cletussam12@yahoo.com>
2023-06-22 14:12:07 +0100

    A function that draws a straight line of (n) characters long

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line of (n) characters long.
+ * @n: the number of underscores to print.
+ */
+void print_line(int n)
+{
+	int line;
+	
+	if (n > 0)
+
+	for (line = 0; line < n; line++)
+
+	putchar('_');
+
+	_putchar('\n');
+}
