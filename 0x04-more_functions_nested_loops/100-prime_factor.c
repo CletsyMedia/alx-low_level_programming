ff952a7a4645a1cc6f8175b61df58043497c58c6
CletsyMedia <Cletussam12@yahoo.com>
2023-06-22 17:31:43 +0100

    A function that prints a triangle using #s

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long long int num;
+	long long int maxNum;
+	long long int i;
+
+	num = 612852475143;
+	maxNum = -1;
+
+	while (num % 2 == 0)
+	{
+		maxNum = 2;
+		num /= 2;
+	}
+
+	for (i = 3; i <= sqrt(num); i = i + 2)
+	{
+		while (num % i == 0)
+		{
+			maxNum = i;
+			num = num / i;
+		}
+	}
+
+	if (num > 2)
+		maxNum = num;
+
+	printf("%ld\n", maxNum);
+
+	return (0);
+}
