3c13119728a5db12d860dc2e47f53ca882aae966
CletsyMedia <Cletussam12@yahoo.com>
2023-06-22 15:46:50 +0100

    Prints a square using #s

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,23 +1,25 @@
 #include "main.h"
-/**
- * print_square - draws a square using terminal lines and #'s
- * @size: determine the number of #s to print in each line.
- *
-*/
 
+/**
+ * print_square - prints a square using #s, followed by a new line;
+ * @size: size of the square
+ */
 void print_square(int size)
 {
-	int sqr1, sqr2;
-
-	if (size > 0)
+	if (size <= 0)
 	{
+		_putchar('\n');
+	} else
+	{
+		int sqr1, sqr2;
+
 		for (sqr1 = 0; sqr1 < size; sqr1++)
 		{
 			for (sqr2 = 0; sqr2 < size; sqr2++)
+			{
 				_putchar('#');
-				_putchar('\n');
+			}
+			_putchar('\n');
 		}
 	}
-	else
-	_putchar('\n');
 }
