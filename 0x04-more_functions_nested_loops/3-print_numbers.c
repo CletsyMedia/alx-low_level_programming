560caaf5443c7f29d744db74aa13b2f67c411247
CletsyMedia <Cletussam12@yahoo.com>
2023-06-22 08:28:41 +0100

    A function to print the digits 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints the digits 0 to 9
+ * followed by a new line
+ * 
+ */
+void print_numbers(void)
+{
+	int i = 0;
+	
+	for (i = 0; i <= 9; ++i)
+	{
+		_putchar(i + '0');
+	}
+		_putchar('\n');
+}
