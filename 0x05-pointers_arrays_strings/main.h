9e5289a7c7212b541e68bd572faff5049ecf109d
CletsyMedia <Cletussam12@yahoo.com>
2023-06-25 16:22:55 +0100

    Updated Prototype header in main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,4 +16,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
 #endif /* MAIN_H */
