4845d0ac1b4e2a0a7a5c4ed2399f2a427b698c0e
CletsyMedia <Cletussam12@yahoo.com>
2023-06-25 18:32:32 +0100

    Swapping two numbers using Pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Write a function that takes a pointer to an int as a parameter
+ * Then update the value it points to 98
+ * @n: the input t
+ * Return: The updated value of n for success
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
