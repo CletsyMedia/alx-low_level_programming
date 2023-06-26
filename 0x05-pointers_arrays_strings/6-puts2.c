87d43979a5f45bb2c1135472b71b27c9f8969ed2
CletsyMedia <Cletussam12@yahoo.com>
2023-06-26 12:08:58 +0100

    This function prints every character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - This function prints every character of a string
+ *
+ * @str: The string parameter input
+ *
+ * Return: Noting
+ */
+void puts2(char *str)
+{
+		int prt;
+
+	for (prt = 0; str[prt] != '\0'; prt++)
+	{
+		if (prt % 2 == 0)
+		_putchar(str[prt]);
+
+	}
+	_putchar('\n');
+}
