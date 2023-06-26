4f7dce8a6529db16ef68a24acd3159a6696a404c
CletsyMedia <Cletussam12@yahoo.com>
2023-06-26 05:38:03 +0100

    A function that prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - This function prints a string in reverse order
+ * followed by a newline
+ * @s: It is the input that prints the string
+ * Return: Nothing
+*/
+void print_rev(char *s)
+{
+	int lenRev;
+
+	/*Finds the length of the string without a null character i.e. using ; */
+	while (s[lenRev] != '\0')
+		lenRev++;
+		lenRev--;
+		;
+
+	/*Prints the char *s from the last index(in reverse) as it decrement(--) */
+	for (--lenRev; lenRev >= 0; lenRev--)
+		_putchar(s[lenRev]);
+		_putchar('\n');
+
+}
