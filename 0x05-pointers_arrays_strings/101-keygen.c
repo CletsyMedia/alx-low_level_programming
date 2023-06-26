b34655a0cf911066bf2f29ffc80f42b2b7c63cf9
CletsyMedia <Cletussam12@yahoo.com>
2023-06-26 20:50:29 +0100

    A program that generate valid password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,21 +13,24 @@ int main(void)
 	int i, sum, n;
 
 	sum = 0;
+
 	srand(time(NULL));
 
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
-		sum += pass[i];
+		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
-		if ((2772 - sum) < 78)
+		if ((2772 - sum) - '0' < 78)
 		{
-			n = 2772 - sum;
+			n = 2772 - sum - '0';
 			sum += n;
 			putchar(n + '0');
 			break;
 		}
 	}
+
+	return (0);
 
 	return (0);
 }
