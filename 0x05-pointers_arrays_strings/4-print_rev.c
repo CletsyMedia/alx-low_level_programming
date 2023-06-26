ffa979366248f99b32adc5f2ae7823e723a54d00
CletsyMedia <Cletussam12@yahoo.com>
2023-06-26 13:04:51 +0100

    Prints string in reverse order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,8 @@ void print_rev(char *s)
 	int lenRev;
 
 	/*Finds the length of the string without a null character i.e. using ; */
-	while (s[lenRev] != '\0')
-	{
-		lenRev++;
-	}
-		lenRev--;
+	for (lenRev = 0; s[lenRev] != '\0'; lenRev++)
+		;
 
 	/*Prints the char *s from the last index(in reverse) as it decrement(--) */
 	for (--lenRev; lenRev >= 0; lenRev--)
