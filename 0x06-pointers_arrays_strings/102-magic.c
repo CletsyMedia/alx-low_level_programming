6f199c1c7e281507fc6cbe60d29c628e14df41dd
CletsyMedia <Cletussam12@yahoo.com>
2023-06-29 23:57:27 +0100

    A function that creates a magic file

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - A function that prints a magic number
+ * main - print ot magic numbers
  *
- * Return: 0;
-*/
+ * Return: 0
+ */
+
 int main(void)
 {
 	int n;
@@ -12,14 +14,15 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+
 	/*
-	* write your line of code here...
-	* Remember:
-	* - you are not allowed to use a
-	* - you are not allowed to modify p
-	* - only one statement
-	* - you are not allowed to code anything else than this line of code
-	*/
+	 * write your line of code here...
+	 * Remember
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
 	p[5] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
