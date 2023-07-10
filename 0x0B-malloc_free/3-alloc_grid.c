6ea1f337e2e2964705d043bb23ecb9f29f323b3e
CletsyMedia <Cletussam12@yahoo.com>
2023-07-10 14:53:04 +0100

    Print 2-D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - This creates a 2-D integer grid
+ * @width: The width of grid
+ * @height: The height of grid
+ * Return: pointer to 2-D integer grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **p;
+
+	i = j = 0;
+	if (height < 1)
+		return (NULL);
+	p = (int **)malloc(height * sizeof(p));
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(width * sizeof(int));
+		if (p[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(p[j]);
+			free(p);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			p[i][j] = 0;
+	}
+	return (p);
+}
