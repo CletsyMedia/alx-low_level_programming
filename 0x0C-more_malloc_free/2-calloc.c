58fded05295227761032c3ff635d8c6ca56e5e86
CletsyMedia <Cletussam12@yahoo.com>
2023-07-13 14:10:32 +0100

    A  function that allocates memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,29 +1,48 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * _calloc -this allocates memory for an initialized buffer
- * @nmemb:The number of memory bytes
- * @size:The number of data type bits
- * Return: pointer to initialized buffer
+ * _memset - Fills memory with a constant byte.
+ *
+ * @s: Pointer to the memory block to fill.
+ * @b: Byte to fill the memory with.
+ * @n: Number of bytes to fill.
+ *
+ * Return: Pointer to the filled memory area.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int idx;
+
+	/* Iterate over each byte and set it to the specified value */
+	for (idx = 0; idx < n; idx++)
+		s[idx] = b;
+
+	return (s);
+}
+
+/**
+ * _calloc - Allocates memory for an array and initializes it to zero.
+ *
+ * @nmemb: The number of elements in the array.
+ * @size: The size (in bytes) of each element.
+ *
+ * Return: Pointer to the newly allocated memory, or NULL if it fails.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *arr;
-	unsigned int indx;
+	char *p;
 
-	/*Check for invalid input*/
+	/* Check for invalid input */
 	if (nmemb == 0 || size == 0)
-	return (NULL);
+		return (NULL);
 
-	/*Allocate memory for the buffer*/
-	arr = malloc(size * nmemb);
-	if (arr == NULL)
-	return (NULL);
+	/* Allocate memory for the array */
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
 
-	/* Initialize the buffer with zeros*/
-	for (indx = 0; indx < nmemb; indx++)
-	arr[indx] = 0;
+	/* Initialize the allocated memory to zero using _memset */
+	_memset(p, 0, nmemb * size);
 
-	return (arr);
+	return (p);
 }
