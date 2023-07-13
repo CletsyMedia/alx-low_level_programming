1400a1ecd4fbb4c1cce8ea923d0bbf466aa79997
CletsyMedia <Cletussam12@yahoo.com>
2023-07-13 14:56:38 +0100

    main.h Modified

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
