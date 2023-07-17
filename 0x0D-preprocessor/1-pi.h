e993d06730f21f46965768724cf501ea19a1eab9
CletsyMedia <Cletussam12@yahoo.com>
2023-07-17 10:48:18 +0100

    A header macro named PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,16 @@
+#ifndef _PI_H
+#define _PI_H
+
+/**
+ * PI - Macro to define the size of a buffer
+ * Value: 3.14159265359
+ * Description: This macro defines the mathematical constant PI with a value
+ * of 3.14159265359. It is used to set a constant value that represents the
+ * ratio of the circumference of a circle to its diameter. This macro can be
+ * used throughout the program, and any occurrence of PI in the code will be
+ * replaced with the value 3.14159265359 during the preprocessing stage.
+ */
+
+#define PI 3.14159265359
+
+#endif /* _PI_H */
