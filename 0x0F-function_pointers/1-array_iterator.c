321294bc3070ff2c735a64179b2536c14d1e9c04
CletsyMedia <Cletussam12@yahoo.com>
2023-07-19 12:21:22 +0100

    A function to a function array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,23 +1,28 @@
 #include "function_pointers.h"
 #include <stddef.h>
-
 /**
- * array_iterator - Execute function on each element of an array.
- * @array: Input array containing integers.
+ * array_iterator - Execute a given function on each element of an array.
+ * @array: Array to iterate.
  * @size: Size of the array.
- * @action: Pointer to a function that takes an integer argument.
+ * @action: Pointer to the function to call.
+ *
+ * Return: Empty if @array and @action are NULL.
  *
  * This function iterates over the elements of the 'array' and applies the
- * function pointed to by 'action' to each element. If 'array' and 'action'
- * are valid (not NULL), the function is executed on each element of the array.
+ * function pointed to by 'action' to each element. If 'array', 'size', and
+ * 'action' are valid (not NULL and size > 0), it uses a while loop to process
+ * each element of the array with the provided function.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array && action)
+	unsigned int idx = 0;
+
+	if (array != NULL && size > 0 && action != NULL)
 	{
-		for (size_t idx = 0; idx < size; idx++)
+		while (idx < size)
 		{
 			action(array[idx]);
+			idx++;
 		}
 	}
 }
