9c9e9b475c8d67ce10ea357fc51b73f24b87ec3f
CletsyMedia <Cletussam12@yahoo.com>
2023-07-19 11:22:10 +0100

    Function prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,19 +1,19 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/* Function prototype for print_name, which takes a char pointer 'name' and a function pointer 'f' as arguments */
+/* takes a char pointer 'name' and a function pointer 'f' */
 void print_name(char *name, void (*f)(char *));
 
-/* Function prototype for array_iterator, which takes an integer pointer 'array', a 'size_t' variable 'size', and a function pointer 'action' as arguments */
+/*takes an int pointer 'array', 'size_t' 'size', and a funct pointer 'action'*/
 void array_iterator(int *array, size_t size, void (*action)(int));
 
-/* Function prototype for int_index, which takes an integer pointer 'array', an integer 'size', and a function pointer 'cmp' as arguments */
+/* takes an int pointer 'array', an int 'size', and a function pointer 'cmp' */
 int int_index(int *array, int size, int (*cmp)(int));
 
 /**
- * enum bool - A custom enumeration called 'bool' representing boolean values.
- * @TRUE: Represents the boolean value 1.
- * @FALSE: Represents the boolean value 0.
+ * enum bool - Custom enum 'bool' representing boolean values.
+ * @TRUE: Represents boolean value 1.
+ * @FALSE: Represents boolean value 0.
  */
 typedef enum bool
 {
