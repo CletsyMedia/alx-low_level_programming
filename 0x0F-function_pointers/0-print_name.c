1dccb5addc8d828ccf6ad9328f292ea3a1bb006f
CletsyMedia <Cletussam12@yahoo.com>
2023-07-19 12:05:51 +0100

    A function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,3 @@
-#include "function_pointers.h"
-
 /**
  * print_name - Prints a name using a function pointer.
  * @name: Input string containing the name to be printed.
@@ -12,6 +10,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	(*f)(name);
 }
+
