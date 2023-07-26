a625dc9aeefbfb1b1f8d4c7d6d8d4b6a5e82249a
CletsyMedia <Cletussam12@yahoo.com>
2023-07-26 22:29:42 +0100

    Function prototypes

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,31 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton tasks
+ */
+/* Node structure */
+typedef struct list_s
+{
+	char *str;	/* Points to a dynamically allocated string */
+	unsigned int len;	/* Length of the string */
+	struct list_s *next;	/* Points to the next node in the list */
+} list_t;
+
+/* Function prototypes */
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif /* LIST_H */
