f1ecb6b0a28e59430459836d2f0a2689ac8af29d
CletsyMedia <Cletussam12@yahoo.com>
2023-08-01 13:27:01 +0100

    Function that frees a list of nodes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,16 +1,14 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Frees the list and sets the head to NULL
+ * free_listint2 - Frees the list and sets head to NULL.
  *
- * @head: Double pointer to the first node of the list
- *
- * Return: Nothing
+ * @head: Pointer to a pointer to the first node.
  *
  * Description:
- *     This function frees the memory occupied by each node in the linked list
- *     starting from the first node. It also sets the head of the list to NULL,
- *     ensuring that the list is empty after freeing all nodes.
+ * The function takes a pointer to a pointer to the first node linked list
+ * It frees the memory occupied by each node in the list
+ * to NULL to ensure it no longer points to the freed memory.
  */
 void free_listint2(listint_t **head)
 {
@@ -18,20 +16,20 @@ void free_listint2(listint_t **head)
 
 	if (head != NULL)
 	{
-		/* Loop through the list, freeing memory for each node */
-		for (current_node = *head; current_node != NULL; current_node = current_node->next)
-		{
-			/* Set temp_node to the current_node before moving to the next node */
-			temp_node = current_node;
+	/* Set current_node to point to the first node of the linked list */
+	current_node = *head;
 
-			/* Move current_node to the next node in the list */
-			/* (already done in the for loop definition) */
+	/* Iterate through the whole linked list */
+	for (temp_node = current_node; temp_node != NULL; temp_node = current_node)
+	{
+	/* Move current_node to the next node before freeing temp_node */
+	current_node = current_node->next;
 
-			/* Free the memory occupied by the temp_node (current_node) */
-			free(temp_node);
-		}
+	/* Free the memory occupied by the current_node (temp_node) */
+	free(temp_node);
+	}
 
-		/* Set the head pointer to NULL to empty the list */
-		*head = NULL;
+	/* After the loop, all nodes have been freed, so set head to NULL */
+	*head = NULL;
 	}
 }
