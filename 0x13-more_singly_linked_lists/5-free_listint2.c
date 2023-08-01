bcd419cbbe022f073622bf54cf1c3998870e3824
CletsyMedia <Cletussam12@yahoo.com>
2023-08-01 13:21:08 +0100

    Function that frees a list of nodes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,35 +1,37 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * free_listint2 - Frees a list and sets its head to NULL
+ * free_listint2 - Frees the list and sets the head to NULL
  *
- * @head: Double pointer to the head of the list
+ * @head: Double pointer to the first node of the list
  *
  * Return: Nothing
  *
  * Description:
- *	This function frees the memory occupied by each node in the linked list
- *	starting from the head. It also sets the head of the list to NULL,
- *	ensuring that the list is empty after freeing all nodes.
+ *     This function frees the memory occupied by each node in the linked list
+ *     starting from the first node. It also sets the head of the list to NULL,
+ *     ensuring that the list is empty after freeing all nodes.
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *Nodetemp, *currNode;
+	listint_t *current_node, *temp_node;
 
 	if (head != NULL)
-
-	/* Set currNode to point to the head of the list */
-	currNode = *head;
-
-	/* Loop through the list, freeing memory for each node */
-	for (currNode = *head; currNode != NULL; currNode = currNode->next)
 	{
-		/* Store the current node in a temporary variable */
-		Nodetemp = currNode;
-		/* Free the memory occupied by the current node */
-		free(Nodetemp);
+		/* Loop through the list, freeing memory for each node */
+		for (current_node = *head; current_node != NULL; current_node = current_node->next)
+		{
+			/* Set temp_node to the current_node before moving to the next node */
+			temp_node = current_node;
+
+			/* Move current_node to the next node in the list */
+			/* (already done in the for loop definition) */
+
+			/* Free the memory occupied by the temp_node (current_node) */
+			free(temp_node);
+		}
+
+		/* Set the head pointer to NULL to empty the list */
+		*head = NULL;
 	}
-	/* Set the head pointer to NULL to empty the list */
-	*head = NULL;
 }
