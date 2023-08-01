b697e3d666738b0221c3a9b2b46d54975980b0ea
CletsyMedia <Cletussam12@yahoo.com>
2023-08-01 15:09:17 +0100

    Prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,24 +8,18 @@
  */
 void free_listp(listp_t **head)
 {
-	/* Declare variables to hold the current and next nodes */
 	listp_t *current;
 	listp_t *next_node;
 
-	/* Check if the 'head' pointer is not NULL */
 	if (head != NULL)
 	{
-		/* Set 'current' to point to the head of the list */
 		current = *head;
-
-		/* Use a loop to traverse the list and free nodes */
-		for (; current != NULL; current = next_node)
+		while (current != NULL)
 		{
-			/* Save the next node before freeing the current one */
 			next_node = current->next;
-			free(current); /* Free the current node */
+			free(current);
+			current = next_node;
 		}
-		/* Set the head of the list to NULL after freeing all nodes */
 		*head = NULL;
 	}
 }
@@ -38,44 +32,37 @@ void free_listp(listp_t **head)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	/* Initialize variables to keep track of the node count and printed nodes */
 	size_t node_count = 0;
 	listp_t *printed_nodes = NULL;
 	listp_t *new_node, *check_node;
 
-	/* Use a loop to traverse the list and print the nodes */
-	for (; head != NULL; head = head->next)
+	while (head != NULL)
 	{
-		/* Allocate memory for a new node to keep track of printed nodes */
 		new_node = malloc(sizeof(listp_t));
 		if (new_node == NULL)
-			exit(98); /* If malloc fails, exit the program with error code 98 */
-		/* Set the 'p' pointer in the new node to point to the current node */
+			exit(98);
+
 		new_node->p = (void *)head;
-
-		/* Link the new node to the existing list of printed nodes */
 		new_node->next = printed_nodes;
-		printed_nodes = new_node; /* Update the list of printed nodes */
+		printed_nodes = new_node;
 
-		/* Check for loops by iterating through the list of printed nodes */
 		check_node = printed_nodes;
-		for (; check_node->next != NULL; check_node = check_node->next)
+		while (check_node->next != NULL)
 		{
-			/* If a loop is found, print the message and free the memory */
+			check_node = check_node->next;
 			if (head == check_node->p)
 			{
 				printf("-> [%p] %d\n", (void *)head, head->n);
 				free_listp(&printed_nodes);
-				return (node_count); /* Return the current node count */
+				return (node_count);
 			}
 		}
 
-		/* If no loop is found, print the address and value of the current node */
 		printf("[%p] %d\n", (void *)head, head->n);
-
-		/* Increment the node count */
+		head = head->next;
 		node_count++;
 	}
+
 	free_listp(&printed_nodes);
 	return (node_count);
 }
