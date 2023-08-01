be3979622b7326437f4d501c1177a8e86967786a
CletsyMedia <Cletussam12@yahoo.com>
2023-08-01 13:15:18 +0100

    Function that frees a list of nodes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 /**
-	* free_listint2 - Frees a list and sets its head to NULL
-	*
-	* @head: double pointer to the head of the list
-	*
-	* Return: Nothing
-	* Description:
-	*	This function frees the memory occupied by each node in the linked list
-	*	starting from the head. It also sets the head of the list to NULL,
-	*	ensuring that the list is empty after freeing all nodes.
-	*/
+ * free_listint2 - Frees a list and sets its head to NULL
+ *
+ * @head: Double pointer to the head of the list
+ *
+ * Return: Nothing
+ *
+ * Description:
+ *	This function frees the memory occupied by each node in the linked list
+ *	starting from the head. It also sets the head of the list to NULL,
+ *	ensuring that the list is empty after freeing all nodes.
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *Nodetemp, *currNode;
@@ -19,14 +20,17 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
+	/* Set currNode to point to the head of the list */
 	currNode = *head;
 
-	/* looping through */
-	for (; currNode != NULL; currNode = currNode->next)
+	/* Loop through the list, freeing memory for each node */
+	for (currNode = *head; currNode != NULL; currNode = currNode->next)
 	{
+		/* Store the current node in a temporary variable */
 		Nodetemp = currNode;
+		/* Free the memory occupied by the current node */
 		free(Nodetemp);
 	}
-
+	/* Set the head pointer to NULL to empty the list */
 	*head = NULL;
 }
