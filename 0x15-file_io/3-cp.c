07077163e9d0ee546194ec2b683f701680ffd700
CletsyMedia <Cletussam12@yahoo.com>
2023-08-07 16:23:15 +0100

    A function that copies the content of another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,60 +1,57 @@
-#include <stdio.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include "main.h"
 
-#define READ_ERROR "Error: Can't read from file %s\n"
-#define WRITE_ERROR "Error: Can't write to %s\n"
-#define CLOSE_ERROR "Error: Can't close fd %d\n"
+#define READ_ERROR "Error: Unable to read from file %s\n"
+#define WRITE_ERROR "Error: Unable to write to %s\n"
 
 /**
- * main - Copies the content of a file to another file.
- * @argc: The number of command-line arguments.
- * @argv: An array containing the command-line arguments.
- *
- * Return: 0 on success, other values on error.
+ * main - Copy content from one file to another.
+ * @argc: The number of arguments.
+ * @argv: Array of argument strings.
+ * Return: Always 0.
  */
-
 int main(int argc, char **argv)
 {
-	int src_fd, dest_fd, close_result, bytes_read, bytes_written;
+	int from, to, close_status;
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(EXIT_FAILURE);
+		dprintf(STDERR_FILENO,
+			"Usage: custom_cp source_file destination_file\n"), exit(97);
 
-	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (dest_fd == -1)
-		dprintf(STDERR_FILENO, WRITE_ERROR, argv[2]), exit(EXIT_FAILURE);
+	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (to == -1)
+		dprintf(STDERR_FILENO, WRITE_ERROR, argv[2]), exit(99);
 
-	src_fd = open(argv[1], O_RDONLY);
-	if (src_fd == -1)
-		dprintf(STDERR_FILENO, READ_ERROR, argv[1]), exit(EXIT_FAILURE);
+	from = open(argv[1], O_RDONLY);
+	if (from == -1)
+		dprintf(STDERR_FILENO, READ_ERROR, argv[1]), exit(98);
 
 	while (1)
 	{
-		bytes_read = read(src_fd, buffer, 1024);
-		if (bytes_read == -1)
-			dprintf(STDERR_FILENO, READ_ERROR, argv[1]), exit(EXIT_FAILURE);
+		int r = read(from, buffer, 1024);
 
-		if (bytes_read > 0)
+		if (r == -1)
+			dprintf(STDERR_FILENO, READ_ERROR, argv[1]), exit(98);
+
+		if (r > 0)
 		{
-			bytes_written = write(dest_fd, buffer, bytes_read);
-			if (bytes_written == -1)
-				dprintf(STDERR_FILENO, WRITE_ERROR, argv[2]), exit(EXIT_FAILURE);
+			int w = write(to, buffer, r);
+
+			if (w == -1)
+				dprintf(STDERR_FILENO, WRITE_ERROR, argv[2]), exit(99);
 		}
 		else
 			break;
 	}
-
-	close_result = close(src_fd);
-	if (close_result == -1)
-		dprintf(STDERR_FILENO, CLOSE_ERROR, src_fd), exit(EXIT_FAILURE);
-
-	close_result = close(dest_fd);
-	if (close_result == -1)
-		dprintf(STDERR_FILENO, CLOSE_ERROR, dest_fd), exit(EXIT_FAILURE);
-
-	return (EXIT_SUCCESS);
+	close_status = close(from);
+	if (close_status == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
+	close_status = close(to);
+	if (close_status == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to), exit(100);
+	return (0);
 }
