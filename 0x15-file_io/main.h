f6b6a53a3bcc10f555caa3dc39167c74e25fdcad
CletsyMedia <Cletussam12@yahoo.com>
2023-09-04 14:49:10 +0100

    Prototypes

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef _IO_
+#define _IO_
+/* Function to read a specified number of letters from a text file */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* Function to create a new file with specified content */
+int create_file(const char *filename, char *text_content);
+
+/* Function to append text content to an existing file */
+int append_text_to_file(const char *filename, char *text_content);
+
+/* Function to calculate the length of a string */
+int _strlen(const char *s);
+
+/* Function to write a character to standard output */
+int _putchar(char c);
+
+#endif /* _IO_ */
