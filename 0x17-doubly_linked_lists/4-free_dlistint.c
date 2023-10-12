8bacd72d7eca43d6d401040153297127f88ae2df
CletsyMedia <Cletussam12@yahoo.com>
2023-10-12 18:19:40 +0100

    Freeing memory

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,18 +1,16 @@
 #include "lists.h"
 
 /**
- * free_dlistint - Frees a linked list.
+ * free_dlistint - Frees a doubly linked list.
  * @head: Pointer to the head of the list.
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *tmp;
+	dlistint_t *nxt_node;
 
-	for (; head != NULL; head = head->next)
+	for (; head; head = nxt_node)
 	{
-	tmp = head;
-	tmp->prev = NULL;
-	tmp->next = NULL;
-	free(tmp);
+	nxt_node = head->next;
+	free(head);
 	}
 }
