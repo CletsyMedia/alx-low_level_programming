43b7f0d7c26540aaa91c4c8e684074582fd5662b
CletsyMedia <Cletussam12@yahoo.com>
2023-10-12 18:42:21 +0100

    Function that inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * insert_dnodeint_at_index - Inserts a new node at a given position in a
- *                          dlistint_t list.
+ *	dlistint_t list.
  * @h: Pointer to a pointer to the head of the list.
  * @idx: Index where the new node should be added (starting from 0).
  * @n: Data to be stored in the new node.
@@ -11,38 +11,38 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-  unsigned int count;
-  dlistint_t *current;
-  dlistint_t *new_node;
+	unsigned int count;
+	dlistint_t *current;
+	dlistint_t *new_node;
 
-  if (!h)
-    return (NULL);
+	if (!h)
+	return (NULL);
 
-  current = *h;
+	current = *h;
 
-  if (idx == 0)
-    return (add_dnodeint(h, n)); /* Insert at the beginning */
+	if (idx == 0)
+	return (add_dnodeint(h, n)); /* Insert at the beginning */
 
-  for (count = 0; current && count < idx - 1; count++)
-  {
-    current = current->next;
-  }
+	for (count = 0; current && count < idx - 1; count++)
+	{
+	current = current->next;
+	}
 
-  if (!current || count != idx - 1)
-    return (NULL); /* Index out of range */
+	if (!current || count != idx - 1)
+	return (NULL); /* Index out of range */
 
-  new_node = malloc(sizeof(dlistint_t));
-  if (!new_node)
-    return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+	return (NULL);
 
-  new_node->n = n;
-  new_node->prev = current;
-  new_node->next = current->next;
+	new_node->n = n;
+	new_node->prev = current;
+	new_node->next = current->next;
 
-  if (current->next)
-    current->next->prev = new_node;
+	if (current->next)
+	current->next->prev = new_node;
 
-  current->next = new_node;
+	current->next = new_node;
 
-  return (new_node);
+	return (new_node);
 }
