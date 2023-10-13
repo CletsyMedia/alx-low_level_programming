0e8e3bf5f49e734489668a4f556895f4450cd0bd
CletsyMedia <Cletussam12@yahoo.com>
2023-10-13 02:25:32 +0100

    Crackme5 keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,46 +3,43 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point. Generates a key for the given username.
- * Generates a key for the given username.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line arguments.
+ * main - generate a key depending on a username for crackme5
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
  *
- * Return: 0 on success, 1 on error.
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
+	unsigned int i, b;
+	size_t len, add;
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char p[7] = "      ";
+
 	if (argc != 2)
 	{
-	printf("Correct usage: ./keygen5 username\n");
-	return (1);
+		printf("Correct usage: ./keygen5 username\n");
+		return (1);
 	}
-
-	char *username = argv[1];
-	size_t len = strlen(username);
-
-	unsigned int values[] = {59, 79, 85, 14, 239, 229};
-	char key[7];
-
-	for (int i = 0; i < 6; i++)
-	{
-	char c = 0;
-
-	for (size_t j = 0; j < len; j++)
-	{
-	c += username[j];
-	if (i == 3)
-	{
-	srand(username[0] ^ values[i]);
-	c = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk"
-	[rand() % 63];
-	}
-	}
-	key[i] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk"
-	[(c ^ values[i]) & 63];
-	}
-	key[6] = '\0';
-
-	printf("%s\n", key);
+	len = strlen(argv[1]);
+	p[0] = l[(len ^ 59) & 63];
+	for (i = 0, add = 0; i < len; i++)
+		add += argv[1][i];
+	p[1] = l[(add ^ 79) & 63];
+	for (i = 0, b = 1; i < len; i++)
+		b *= argv[1][i];
+	p[2] = l[(b ^ 85) & 63];
+	for (b = argv[1][0], i = 0; i < len; i++)
+		if ((char)b <= argv[1][i])
+			b = argv[1][i];
+	srand(b ^ 14);
+	p[3] = l[rand() & 63];
+	for (b = 0, i = 0; i < len; i++)
+		b += argv[1][i] * argv[1][i];
+	p[4] = l[(b ^ 239) & 63];
+	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
+		b = rand();
+	p[5] = l[(b ^ 229) & 63];
+	printf("%s\n", p);
 	return (0);
 }
