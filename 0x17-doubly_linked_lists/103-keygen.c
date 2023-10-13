ad2498f908fcbd42f3609391d760ccdd9f1ce699
CletsyMedia <Cletussam12@yahoo.com>
2023-10-13 02:36:08 +0100

    Crackme5 keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,43 +3,69 @@
 #include <stdlib.h>
 
 /**
- * main - generate a key depending on a username for crackme5
- * @argc: number of arguments passed
- * @argv: arguments passed to main
+ * generate_key - Generates a key based on the username.
+ * main - Entry point
  *
- * Return: 0 on success, 1 on error
+ * @username: The input username.
+ * @key: The generated key to be filled in.
+ *
+ * Returns: The generated key
  */
+void generate_key(const char *username, char *key)
+{
+	unsigned int len = strlen(username);
+	unsigned int values[] = {59, 79, 85, 14, 239, 229};
+	unsigned int i, b;
+	size_t add;
+
+	key[0] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk"
+	[(len ^ 59) & 63];
+
+	for (i = 0, add = 0; i < len; i++)
+	add += username[i];
+	key[1] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk"
+	[(add ^ 79) & 63];
+
+	for (i = 0, b = 1; i < len; i++)
+	b *= username[i];
+	key[2] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk"
+	[(b ^ 85) & 63];
+
+	for (b = username[0], i = 0; i < len; i++)
+	{
+	if ((char)b <= username[i])
+	b = username[i];
+	}
+	srand(b ^ 14);
+	key[3] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk"
+	[rand() % 63];
+
+	for (b = 0, i = 0; i < len; i++)
+	b += username[i] * username[i];
+	key[4] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk"
+	[(b ^ 239) & 63];
+
+	for (b = 0, i = 0; (char)i < username[0]; i++)
+	b = rand();
+	key[5] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk"
+	[(b ^ 229) & 63];
+
+	key[6] = '\0';
+}
+
 int main(int argc, char *argv[])
 {
-	unsigned int i, b;
-	size_t len, add;
-	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	char p[7] = "      ";
-
 	if (argc != 2)
 	{
-		printf("Correct usage: ./keygen5 username\n");
-		return (1);
+	printf("Correct usage: ./keygen5 username\n");
+	return (1);
 	}
-	len = strlen(argv[1]);
-	p[0] = l[(len ^ 59) & 63];
-	for (i = 0, add = 0; i < len; i++)
-		add += argv[1][i];
-	p[1] = l[(add ^ 79) & 63];
-	for (i = 0, b = 1; i < len; i++)
-		b *= argv[1][i];
-	p[2] = l[(b ^ 85) & 63];
-	for (b = argv[1][0], i = 0; i < len; i++)
-		if ((char)b <= argv[1][i])
-			b = argv[1][i];
-	srand(b ^ 14);
-	p[3] = l[rand() & 63];
-	for (b = 0, i = 0; i < len; i++)
-		b += argv[1][i] * argv[1][i];
-	p[4] = l[(b ^ 239) & 63];
-	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
-		b = rand();
-	p[5] = l[(b ^ 229) & 63];
-	printf("%s\n", p);
+
+	char *username = argv[1];
+	char key[7];
+
+	generate_key(username, key);
+
+	printf("%s\n", key);
 	return (0);
 }
