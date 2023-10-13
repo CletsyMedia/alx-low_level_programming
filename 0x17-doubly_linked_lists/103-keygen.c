e509e219911f25458c981d2dbde759032f711a78
CletsyMedia <Cletussam12@yahoo.com>
2023-10-13 03:12:54 +0100

    Crackme5 keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,97 +3,43 @@
 #include <stdlib.h>
 
 /**
- * generate_key - Generate a key char based on input string, shift, and mask
- * @input: Input string
- * @shift: Shift value
- * @mask: Mask value
- *
- * This function calculates a key character based on the input string and
- * specified shift and mask values.
- *
- * Return: Generated key character
- */
-char generate_key(char *input, int shift, int mask);
-
-/**
- * main - Entry point of the program
- * @argc: Number of arguments
- * @argv: Array of arguments
- *
- * This function is the entry point of the program. It generates a key based on
- * a username and displays it.
+ * main - generate a key depending on a username for crackme5
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
  *
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
+	unsigned int i, b;
+	size_t len, add;
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char p[7] = "	";
+
 	if (argc != 2)
 	{
 	printf("Correct usage: ./keygen5 username\n");
 	return (1);
 	}
-
-	char *username = argv[1];
-	char key[7] = "	";
-
-	key[0] = generate_key(username, 59, 63);
-	key[1] = generate_key(username, 79, 63);
-	key[2] = generate_key(username, 85, 63);
-
-	char max_char = username[0];
-
-	for (int i = 0; username[i] != '\0'; i++)
-	{
-	if (username[i] <= max_char)
-	{
-	max_char = username[i];
-	}
-	}
-	srand(max_char ^ 14);
-	key[3] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk"
-	[rand() & 63];
-
-	int b = 0;
-
-	for (int i = 0; username[i] != '\0'; i++)
-	{
-	b += username[i] * username[i];
-	}
-	key[4] = generate_key(username, 239, 63);
-
-	int rand_seed = 0;
-
-	for (int i = 0; i < username[0]; i++)
-	{
-	rand_seed = rand();
-	}
-	key[5] = generate_key(username, 229, 63);
-
-	printf("%s\n", key);
+	len = strlen(argv[1]);
+	p[0] = l[(len ^ 59) & 63];
+	for (i = 0, add = 0; i < len; i++)
+	add += argv[1][i];
+	p[1] = l[(add ^ 79) & 63];
+	for (i = 0, b = 1; i < len; i++)
+	b *= argv[1][i];
+	p[2] = l[(b ^ 85) & 63];
+	for (b = argv[1][0], i = 0; i < len; i++)
+	if ((char)b <= argv[1][i])
+	b = argv[1][i];
+	srand(b ^ 14);
+	p[3] = l[rand() & 63];
+	for (b = 0, i = 0; i < len; i++)
+	b += argv[1][i] * argv[1][i];
+	p[4] = l[(b ^ 239) & 63];
+	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
+	b = rand();
+	p[5] = l[(b ^ 229) & 63];
+	printf("%s\n", p);
 	return (0);
-}
-
-/**
- * generate_key - Generate a key char based on input string, shift, and mask
- * @input: Input string
- * @shift: Shift value
- * @mask: Mask value
- *
- * This function calculates a key character based on the input string and
- * specified shift and mask values.
- *
- * Return: Generated key character
- */
-char generate_key(char *input, int shift, int mask)
-{
-	int len = strlen(input);
-	int result = shift;
-
-	for (int i = 0; input[i] != '\0'; i++)
-	{
-	result += input[i];
-	}
-
-	return ("A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk"
-	[(result ^ mask) & 63]);
 }
