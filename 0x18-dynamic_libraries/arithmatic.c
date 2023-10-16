1727beedbb5a6ebeaae3e2be4b6d22fbd13f84dd
CletsyMedia <Cletussam12@yahoo.com>
2023-10-16 22:12:24 +0100

    Updates

---
+++ 0x18-dynamic_libraries/arithmatic.c
@@ -0,0 +1,55 @@
+#include "arithmatic.h"
+/**
+ *add - function that add two integer
+ *@a: takes value of a
+ *@b: takes the value of b
+ *Return: return the sum of two integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *add - function that subtwo integer
+ *@a: takes value of a
+ *@b: takes the value of b
+ *Return: return the subof two integer
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *add - function that multwo integer
+ *@a: takes value of a
+ *@b: takes the value of b
+ *Return: return the mul of two integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *add - function that div two integer
+ *@a: takes value of a
+ *@b: takes the value of b
+ *Return: return the div of two integer
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *add - function that mod two integer
+ *@a: takes value of a
+ *@b: takes the value of b
+ *Return: return the mod of two integer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/arithmatic.h
@@ -0,0 +1,5 @@
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
