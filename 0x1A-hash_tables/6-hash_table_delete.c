c6404dca62e4eb4b85f821f5eca8737b53205241
CletsyMedia <Cletussam12@yahoo.com>
2023-11-02 13:02:25 +0100

    Testing for collision

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,21 +9,20 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *tmp1;
-	hash_node_t *tmp2;
 
 	if (ht == NULL)
 		return;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		tmp1 = ht->array[i];
-		while ((tmp2 = tmp1) != NULL)
+		while (ht->array[i] != NULL)
 		{
-			tmp1 = tmp1->next;
-			free(tmp2->key);
-			free(tmp2->value);
-			free(tmp2);
+			hash_node_t *current = ht->array[i];
+
+			ht->array[i] = ht->array[i]->next;
+			free(current->key);
+			free(current->value);
+			free(current);
 		}
 	}
 	free(ht->array);
