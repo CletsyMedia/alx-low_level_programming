72fbb02c40859e5de71eea333ce8d557d494e19e
CletsyMedia <Cletussam12@yahoo.com>
2023-11-02 19:50:18 +0100

    Creating hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	hash_node_t **ht_arr;
+	hash_node_t **array;
 	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
@@ -16,17 +16,21 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	ht->size = size;
-	ht_arr = malloc(sizeof(hash_node_t *) * size);
+	array = malloc(sizeof(hash_node_t *) * size);
 
-	if (ht_arr == NULL)
+	if (array == NULL)
 	{
 		free(ht);
 		return (NULL);
 	}
 
-	for (i = 0; i < size; i++)
-		ht_arr[i] = NULL;
+	i = 0;
+	while (i < size)
+	{
+		array[i] = NULL;
+		i++;
+	}
 
-	ht->array = ht_arr;
+	ht->array = array;
 	return (ht);
 }
