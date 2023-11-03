caa76bbeedb34d746d663b875b7c977f8c03682e
CletsyMedia <Cletussam12@yahoo.com>
2023-11-03 04:17:47 +0100

    Creating has table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,30 +1,42 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
- *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
+ * hash_table_create - creates a hash table
+ * @size: the size of the array to be created
+ * Return: a pointer to the newly created hash table, or NULL on failure
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *hash_table = NULL;
+	unsigned long int inno;
 
 	if (size == 0)
-		return (NULL);
-
-	table = calloc(1, sizeof(hash_table_t));
-	if (table == NULL)
-		return (NULL);
-
-	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *));
-	if (table->array == NULL)
 	{
-		free(table);
 		return (NULL);
 	}
-	return (table);
-}
 
+	/* Allocate memory for hash table structure */
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+	{
+		return (NULL);
+	}
+
+	/* Allocate memory for array of hash nodes */
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	hash_table->size = size;
+
+	/* Initialize all hash node pointers to NULL */
+	for (inno = 0; inno < size; inno++)
+	{
+		hash_table->array[inno] = NULL;
+	}
+
+	return (hash_table);
+}
