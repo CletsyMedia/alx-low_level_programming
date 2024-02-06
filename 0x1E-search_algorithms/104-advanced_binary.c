bd283574ed431cdce9a1977034dcd01bc9aa43b2
CletsyMedia <Cletussam12@yahoo.com>
2024-02-06 22:45:11 +0100

    Function that searches for a value in a sorted array of int

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,47 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * advanced_binary - Searches for a value in a sorted array of integers
+ *	using the advanced binary search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: Index where the value is located, or -1 if not found
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t left = 0, right = size - 1;
+	size_t mid = 0;
+	int index = -1;
+
+	if (array == NULL)
+	return (-1);
+
+	while (left <= right)
+	{
+	printf("Searching in array: ");
+	for (size_t i = left; i <= right; i++)
+	{
+	printf("%d", array[i]);
+	if (i < right)
+	printf(", ");
+	}
+	printf("\n");
+
+	mid = (left + right) / 2;
+
+	if (array[mid] == value)
+	{
+	index = mid;
+	right = mid - 1;
+	}
+	else if (array[mid] < value)
+	left = mid + 1;
+	else
+	right = mid - 1;
+	}
+
+	return (index);
+}
--- 0x1E-search_algorithms/main.c
+++ 0x1E-search_algorithms/main.c
@@ -2,20 +2,14 @@
 #include <stdlib.h>
 #include "search_algos.h"
 
-/**
- * main - Entry point
- *
- * Return: Always EXIT_SUCCESS
- */
 int main(void)
 {
-    int array[] = {
-        0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 54, 61, 62, 76, 99
-    };
-    size_t size = sizeof(array) / sizeof(array[0]);
+  int array[] = {
+      0, 1, 2, 5, 5, 6, 6, 7, 8, 9};
+  size_t size = sizeof(array) / sizeof(array[0]);
 
-    printf("Found %d at index: %d\n\n", 62, exponential_search(array, size, 62));
-    printf("Found %d at index: %d\n\n", 3, exponential_search(array, size, 3));
-    printf("Found %d at index: %d\n", 999, exponential_search(array, size, 999));
-    return (EXIT_SUCCESS);
+  printf("Found %d at index: %d\n\n", 8, advanced_binary(array, size, 8));
+  printf("Found %d at index: %d\n\n", 5, advanced_binary(array, size, 5));
+  printf("Found %d at index: %d\n", 999, advanced_binary(array, size, 999));
+  return (EXIT_SUCCESS);
 }
