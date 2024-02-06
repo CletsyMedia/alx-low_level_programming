be71356c24cfe36459c83a89a45de0177a2b4af2
CletsyMedia <Cletussam12@yahoo.com>
2024-02-06 21:38:54 +0100

    Search for a val using expo search algo

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,37 +1,73 @@
 #include "search_algos.h"
+#include <stdio.h>
+
 /**
- * exponential_search - function that searches for a value in a sorted
- * array of integers using the Exponential search algorithm
- * @array : pointer to the first element of the array to search in.
- * @size : the number of elements in array.
- * @value : value to search for.
- * Return: index of value or -1 if not found.
-*/
+ * print_range - Print the range of values being searched.
+ * @array: The array being searched.
+ * @start: The starting index of the range.
+ * @end: The ending index of the range.
+ */
+void print_range(int *array, size_t start, size_t end)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+	printf("%d", array[i]);
+	if (i != end)
+	printf(", ");
+	}
+	printf("\n");
+}
+
+/**
+ * exponential_search - Searches for a value in a sorted array of integers
+ * using the Exponential search algorithm.
+ * @array: A pointer to the first element of the array to search in.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ * Return: The index where the value is located, or -1 if not found.
+ */
 int exponential_search(int *array, size_t size, int value)
 {
-  size_t index = 1, r = 0, move;
+	size_t bound;
+	size_t start;
+	size_t end;
 
-	if (array)
+	if (array == NULL)
+	return (-1);
+
+	if (size == 0)
+	return (-1);
+
+	bound = 1;
+
+	while (bound < size && array[bound] < value)
 	{
-		while (index < size && array[index] < value)
-		{
-			printf("Value checked array[%lu] = [%d]\n", index, array[index]);
-			index *= 2;
-		}
-		r = index < size - 1 ? index : size - 1;
-		index /= 2;
-		printf("Value found between indexes [%lu] and [%lu]\n", index, r);
-		while (index <= r)
-		{
-			move = (index + r) / 2;
-			printf("Value checked array[%lu] = [%d]\n", move, array[move]);
-			if (array[move] == value)
-				return (move);
-			if (array[move] < value)
-				index = move + 1;
-			else
-				r = move - 1;
-		}
+	printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+	bound *= 2;
 	}
+
+	start = bound / 2;
+	end = (bound < size) ? bound : size - 1;
+
+	printf("Value found between indexes [%lu] and [%lu]\n", start, end);
+	print_range(array, start, end);
+
+	while (start <= end)
+	{
+	size_t mid = (start + end) / 2;
+
+	printf("Searching in array: ");
+	print_range(array, start, end);
+	if (array[mid] == value)
+	return (mid);
+	else if (array[mid] < value)
+	start = mid + 1;
+	else
+	end = mid - 1;
+	}
+
 	return (-1);
 }
