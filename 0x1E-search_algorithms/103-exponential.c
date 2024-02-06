f86b17b1ffc77e257d2af6a242a1caa2f062c8f3
CletsyMedia <Cletussam12@yahoo.com>
2024-02-06 21:44:18 +0100

    Search for a val using expo search algo

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -31,15 +31,18 @@ void print_range(int *array, size_t start, size_t end)
  */
 int exponential_search(int *array, size_t size, int value)
 {
+	if (array == NULL)
+	{
+	return (-1);
+	}
+	if (size == 0)
+	{
+	return (-1);
+	}
+
 	size_t bound;
 	size_t start;
 	size_t end;
-
-	if (array == NULL)
-	return (-1);
-
-	if (size == 0)
-	return (-1);
 
 	bound = 1;
 
@@ -54,20 +57,25 @@ int exponential_search(int *array, size_t size, int value)
 
 	printf("Value found between indexes [%lu] and [%lu]\n", start, end);
 	print_range(array, start, end);
-
 	while (start <= end)
 	{
 	size_t mid = (start + end) / 2;
 
 	printf("Searching in array: ");
 	print_range(array, start, end);
+
 	if (array[mid] == value)
+	{
 	return (mid);
+	}
 	else if (array[mid] < value)
+	{
 	start = mid + 1;
+	}
 	else
+	{
 	end = mid - 1;
 	}
-
+	}
 	return (-1);
 }
