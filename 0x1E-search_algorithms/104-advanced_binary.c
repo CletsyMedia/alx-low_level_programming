5f4ccd4803b75c6333ece7969901158a3875fbee
CletsyMedia <Cletussam12@yahoo.com>
2024-02-06 23:07:20 +0100

    Function that searches for a value in a sorted array of int

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -2,47 +2,6 @@
 #include <stdio.h>
 
 /**
- * advanced_binary_recursive - Recursive helper function for advanced_binary
- * @array: Pointer to the first element of the array to search in
- * @left: Left index of the subarray to search in
- * @right: Right index of the subarray to search in
- * @value: Value to search for
- *
- * Return: Index where the value is located, or -1 if not found
- */
-int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
-{
-	size_t i;
-	size_t mid;
-
-	if (left > right)
-	return (-1);
-
-	printf("Searching in array: ");
-	for (i = left; i <= right; i++)
-	{
-	printf("%d", array[i]);
-	if (i < right)
-	printf(", ");
-	}
-	printf("\n");
-
-	mid = (left + right) / 2;
-
-	if (array[mid] == value)
-	{
-	if (mid == left || array[mid - 1] != value)
-	return (mid);
-	return (advanced_binary_recursive(array, left, mid, value));
-	}
-	else if (array[mid] < value)
-	return (advanced_binary_recursive(array, mid + 1, right, value));
-	else
-	return (advanced_binary_recursive(array, left, mid - 1, value));
-}
-
-
-/**
  * advanced_binary - Searches for a value in a sorted array of integers
  *	using the advanced binary search algorithm
  * @array: Pointer to the first element of the array to search in
@@ -53,8 +12,40 @@ int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (array == NULL || size == 0)
-		return (-1);
+	size_t left = 0, right = size - 1, mid, i;
 
-	return (advanced_binary_recursive(array, 0, size - 1, value));
+	if (array == NULL || size == 0)
+	return (-1);
+
+	while (left <= right)
+	{
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+	printf("%d", array[i]);
+	if (i < right)
+	printf(", ");
+	}
+	printf("\n");
+
+	mid = left + (right - left) / 2;
+
+	if (array[mid] == value)
+	{
+	if (mid == left || array[mid - 1] != value)
+	return (mid);
+	else
+	right = mid;
+	}
+	else if (array[mid] < value)
+	{
+	left = mid + 1;
+	}
+	else
+	{
+	right = mid - 1;
+	}
+	}
+
+	return (-1);
 }
