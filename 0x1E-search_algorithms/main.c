e8ad4afe28b3f6c7af26a30e9259f47b24c6f085
CletsyMedia <Cletussam12@yahoo.com>
2024-02-06 23:44:15 +0100

    Updates

--- 0x1E-search_algorithms/main.c
+++ 0x1E-search_algorithms/main.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include "search_algos.h"
 
-listint_t *create_list(int *array, size_t size);
-void print_list(const listint_t *list);
-void free_list(listint_t *list);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
 
 /**
  * main - Entry point
@@ -13,22 +13,22 @@ void free_list(listint_t *list);
  */
 int main(void)
 {
-    listint_t *list, *res;
+    skiplist_t *list, *res;
     int array[] = {
         0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 53, 61, 62, 76, 99
     };
     size_t size = sizeof(array) / sizeof(array[0]);
 
-    list = create_list(array, size);
-    print_list(list);
+    list = create_skiplist(array, size);
+    print_skiplist(list);
 
-    res =  jump_list(list, size, 53);
+    res =  linear_skip(list, 53);
     printf("Found %d at index: %lu\n\n", 53, res->index);
-    res =  jump_list(list, size, 2);
+    res =  linear_skip(list, 2);
     printf("Found %d at index: %lu\n\n", 2, res->index);
-    res =  jump_list(list, size, 999);
+    res =  linear_skip(list, 999);
     printf("Found %d at index: %p\n", 999, (void *) res);
 
-    free_list(list);
+    free_skiplist(list);
     return (EXIT_SUCCESS);
 }
