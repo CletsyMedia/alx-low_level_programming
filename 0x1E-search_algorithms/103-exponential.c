0d98f9ef9ca6526e6530044af8eba3dd0adfa18c
CletsyMedia <Cletussam12@yahoo.com>
2024-02-06 22:38:13 +0100

    Search for a val using expo search algo

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -2,43 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_range - Print the range of values being searched.
- * @array: The array being searched.
- * @start: The starting index of the range.
- * @end: The ending index of the range.
- */
-void print_range(int *array, size_t start, size_t end)
-{
-	size_t i;
-
-	printf("Searching in array: ");
-	for (i = start; i <= end; i++)
-	{
-	printf("%d", array[i]);
-	if (i != end)
-	printf(", ");
-	}
-	printf("\n");
-}
-
-
-/**
  * exponential_search - Searches for a value in a sorted array of integers
- * using the Exponential search algorithm.
- * @array: A pointer to the first element of the array to search in.
- * @size: The number of elements in the array.
- * @value: The value to search for.
- * Return: The index where the value is located, or -1 if not found.
+ *	using the Exponential search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: Index where the value is located, or -1 if not found
  */
 int exponential_search(int *array, size_t size, int value)
 {
 	size_t bound = 1;
-	size_t start, end;
+	size_t left, right;
+	size_t mid, i;
 
 	if (array == NULL)
-	return (-1);
-
-	if (size == 0)
 	return (-1);
 
 	while (bound < size && array[bound] < value)
@@ -47,29 +25,31 @@ int exponential_search(int *array, size_t size, int value)
 	bound *= 2;
 	}
 
-	start = bound / 2;
-	end = (bound < size) ? bound : size - 1;
+	left = bound / 2;
+	right = (bound < size - 1) ? bound : size - 1;
 
-	printf("Value found between indexes [%lu] and [%lu]\n", start, end);
-	print_range(array, start, end);
-	while (start <= end)
+	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
+	while (left <= right)
 	{
-	size_t mid = (start + end) / 2;
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+	printf("%d", array[i]);
+	if (i < right)
+	printf(", ");
+	}
+	printf("\n");
+
+	mid = (left + right) / 2;
 
 	if (array[mid] == value)
 	return (mid);
-	else if (array[mid] < value)
-	{
-	printf("Searching in array: ");
-	print_range(array, mid + 1, end);
-	start = mid + 1;
-	}
+
+	if (array[mid] < value)
+	left = mid + 1;
 	else
-	{
-	printf("Searching in array: ");
-	print_range(array, start, mid - 1);
-	end = mid - 1;
+	right = mid - 1;
 	}
-	}
+
 	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <stddef.h>
 
 /* Linear search algorithm */
 int linear_search(int *array, size_t size, int value);
