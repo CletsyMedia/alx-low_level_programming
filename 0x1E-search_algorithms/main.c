508f379ad78b72613d5ed8708d2e74894526ebf7
CletsyMedia <Cletussam12@yahoo.com>
2024-02-06 19:13:43 +0100

    Linear search algo

--- 0x1E-search_algorithms/main.c
+++ 0x1E-search_algorithms/main.c
@@ -10,12 +10,12 @@
 int main(void)
 {
     int array[] = {
-        0, 0, 1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 8, 9, 9
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
     };
     size_t size = sizeof(array) / sizeof(array[0]);
 
-    printf("Found %d at index: %d\n\n", 3, interpolation_search(array, size, 3));
-    printf("Found %d at index: %d\n\n", 7, interpolation_search(array, size, 7));
-    printf("Found %d at index: %d\n", 999, interpolation_search(array, size, 999));
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
     return (EXIT_SUCCESS);
 }
