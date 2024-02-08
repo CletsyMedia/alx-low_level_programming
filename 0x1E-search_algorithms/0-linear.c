31f835f1d4df5703fe60f9bb026631a74eab765a
CletsyMedia <Cletussam12@yahoo.com>
2024-02-08 01:56:28 +0100

    Learning to write linear search algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,20 +11,18 @@
  * Return: The first index where value is located,
  *	or -1 if value is not present in array or array is NULL
  */
-int linear_search(const int *array, size_t size, int value)
+int linear_search(int *array, size_t size, int value)
 {
-	size_t index;
+	size_t i;
 
 	if (array == NULL)
 	return (-1);
 
-	for (index = 0; index < size; index++)
+	for (i = 0; i < size; i++)
 	{
-	if (array[index] == value)
-	{
-	printf("Value checked array[%lu] = [%d]\n", index, array[index]);
-	return (index);
-	}
+	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	if (array[i] == value)
+	return (i);
 	}
 
 	return (-1);
