de4ec76924508ef561b4cb9ad88f698283fe69e6
CletsyMedia <Cletussam12@yahoo.com>
2024-02-08 01:50:19 +0100

    Learning to write linear search algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,31 +1,29 @@
+#include <stdio.h>
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of
- * integers using the Linear search algorithm
+ * linear_search - Searches for a value in an array of integers
+ *	using the Linear search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
  *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: Always EXIT_SUCCESS
+ * Return: The first index where value is located,
+ *	or -1 if value is not present in array or array is NULL
  */
-
 int linear_search(int *array, size_t size, int value)
 {
-	size_t x;
+	size_t i;
 
 	if (array == NULL)
+	return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		return (-1);
+	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	if (array[i] == value)
+	return (i);
 	}
 
-	for (x = 0; x < size; x++)
-	{
-		printf("Value checked array[%li] = [%i]\n", x, array[x]);
-		if (array[x] == value)
-		{
-			return (x);
-		}
-	}
 	return (-1);
 }
--- 0x1E-search_algorithms/main.c
+++ 0x1E-search_algorithms/main.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 #include "search_algos.h"
 
-skiplist_t *create_skiplist(int *array, size_t size);
-void print_skiplist(const skiplist_t *list);
-void free_skiplist(skiplist_t *list);
-
 /**
  * main - Entry point
  *
@@ -13,22 +9,13 @@ void free_skiplist(skiplist_t *list);
  */
 int main(void)
 {
-    skiplist_t *list, *res;
     int array[] = {
-        0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 53, 61, 62, 76, 99
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
     };
     size_t size = sizeof(array) / sizeof(array[0]);
 
-    list = create_skiplist(array, size);
-    print_skiplist(list);
-
-    res =  linear_skip(list, 53);
-    printf("Found %d at index: %lu\n\n", 53, res->index);
-    res =  linear_skip(list, 2);
-    printf("Found %d at index: %lu\n\n", 2, res->index);
-    res =  linear_skip(list, 999);
-    printf("Found %d at index: %p\n", 999, (void *) res);
-
-    free_skiplist(list);
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
     return (EXIT_SUCCESS);
 }
